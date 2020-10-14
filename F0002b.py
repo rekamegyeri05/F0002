f34f7fa019253f718d6b9307bb057b3b9b2725ca
rekamegyeri05 <71382672+rekamegyeri05@users.noreply.github.com>
2020-10-14 14:29:37 +0200

    Add files via upload

---
+++ F0002b.py
@@ -0,0 +1,13 @@
+#F0002b: Mi lesz kiírva a program kimenetének utolsó sorában?
+
+
+cica = 'kutya'
+print(cica)
+kutya = 'egér'
+egér = 'cica'
+print('kutya')
+cica = 'tyúk'
+print(egér)
+kutya = cica
+print('kutya')
+print(kutya)
---
+++ F0002c.py
@@ -0,0 +1,7 @@
+#F0002c: Hány sornyi kiírás, azaz szöveg és üres sor kerül a képernyőre, ha majd egyszer lefuttatod ezt a programot?
+
+print('Első sor\n')
+print('\nNos?')
+print('Azaz?\n')
+print('\nAkkor most mennyi?\n\n')
+print('Utolsó sor')
