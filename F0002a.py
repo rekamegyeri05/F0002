ece82884ac01e4fb350065b0640f7df0b4455c51
rekamegyeri05 <71382672+rekamegyeri05@users.noreply.github.com>
2020-10-14 14:11:46 +0200

    Add files via upload

---
+++ F0002a.py
@@ -0,0 +1,9 @@
+#Melyik állat nevét nem írja ki a program? (Csak a válasz megadását követően ér lefuttatni a programot:))
+
+cica = 'kutya'
+kutya = 'egér'
+egér = 'cica'
+print(cica)
+print('kutya')
+print(egér)
+print('cica')
